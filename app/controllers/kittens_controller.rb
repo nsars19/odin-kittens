0735a2d3033465e1b1521fd9a41082e8e268e671
Nick <nsardonini@gmail.com>
2020-08-25 10:59:31 -0400

    add controller actions for Kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,45 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find params[:id]
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new
+
+    if @kitten.save
+      redirect_to kitten_path @kitten
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find params[:id]
+  end
+
+  def update
+    @kitten = Kitten.find params[:id]
+
+    @kitten = @kitten.update
+
+    if @kitten.save
+      redirect_to kitten_path @kitten
+    else
+      render :update
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find params[:id]
+
+    @kitten.destroy
+  end
 end
