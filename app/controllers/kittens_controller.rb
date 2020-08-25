3ef3f82303b9e09f769e45406d079810df230a08
Nick <nsardonini@gmail.com>
2020-08-25 16:57:38 -0400

    allow json rendering for #index and #show to enable use of app as an API

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,10 +1,20 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @kittens }
+    end
   end
 
   def show
     @kitten = Kitten.find params[:id]
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @kitten }
+    end
   end
 
   def new
@@ -12,10 +22,11 @@ class KittensController < ApplicationController
   end
 
   def create
-    @kitten = Kitten.new
+    @kitten = Kitten.new kitten_params
 
     if @kitten.save
       redirect_to kitten_path @kitten
+      flash[:notice] = "Kitten created successfully!"
     else
       render :new
     end
@@ -28,10 +39,9 @@ class KittensController < ApplicationController
   def update
     @kitten = Kitten.find params[:id]
 
-    @kitten = @kitten.update
-
-    if @kitten.save
+    if @kitten.update kitten_params
       redirect_to kitten_path @kitten
+      flash[:notice] = "Kitten Updated"
     else
       render :update
     end
@@ -41,5 +51,13 @@ class KittensController < ApplicationController
     @kitten = Kitten.find params[:id]
 
     @kitten.destroy
+
+    redirect_to kittens_path
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
